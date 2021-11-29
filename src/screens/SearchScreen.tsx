2b3b9409f95461c1f948a0dbe93309eb0b10d5ef
Muhammad Mujtaba <mmujtabam@hotmail.com>
2021-11-29 10:12:09 +0500

    Making Hooks Reusable

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,28 +1,32 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "san jose",
-      },
+  const filterResultsByPrice = (price : any) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter((result : any) => {
+      return result.price === price;
     });
-    setResults(response.data.businesses);
   };
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
